286b9376966e5ef508cb1308bbab1269fc687fec
Arold Mauger <aroldmauger2022@gmail.com>
2024-12-03 13:47:53 +0100

    feat(database)) : creating database connection and using PDO

--- app/src/Controller/AbstractController.php
+++ app/src/Controller/AbstractController.php
@@ -3,15 +3,21 @@
 namespace App\Controller;
 
 use Twig\Environment;
+use PDO;
+
 
 abstract class AbstractController
 {
-    private readonly Environment $twig;
+    protected Environment $twig;
+    protected PDO $pdo;
 
     public function __construct()
     {
         $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../template');
         $this->twig = new \Twig\Environment($loader);
+
+        $this->pdo = require __DIR__ . './../config/bdd.php';
+
     }
 
     protected function render(string $view, array $params = []): string {
